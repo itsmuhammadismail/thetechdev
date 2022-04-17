fbed46a11ab5584a1c1a988188e4c4b0a0628e3f
Muhammad Ismail <itsmuhammadismail@gmail.com>
2022-04-17 16:53:22 +0500

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import AOS from "aos";
 
 import "aos/dist/aos.css";
 // import "../scss/style.scss";
+import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -15,7 +17,32 @@ function MyApp({ Component, pageProps }) {
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-QRP84F44J8`}
+      />
+
+      <Script id="gtm-script" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-QRP84F44J8');
+    `}
+      </Script>
+
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="AQ_XmLCWht2lks64FHxA-8_wiza45xHB1eCOKmdHLMU"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
